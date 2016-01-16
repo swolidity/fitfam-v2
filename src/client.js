8722c2019a3de2fa3e34308ed1473073bc352802
Andy Kay <andykay.me@gmail.com>
2016-01-16 08:36:39 -0500

    Add browser history

--- src/client.js
+++ src/client.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Router } from 'react-router'
+import createBrowserhistory from 'history/lib/createBrowserHistory'
 import routes from './routes'
 
-render(<Router>{routes}</Router>, document.getElementById('app'))
+const history = createBrowserhistory()
+
+render(<Router history={history}>{routes}</Router>, document.getElementById('app'))
