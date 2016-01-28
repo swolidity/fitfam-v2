9eeaa03eaeef43c44f5394bb47310bcaeb1d4f51
adavkay <andykay.me@gmail.com>
2016-01-28 16:52:11 -0500

    switch static class props to properties on constructor because babel now requires a semicolon after static class properties

--- src/components/Html/Html.js
+++ src/components/Html/Html.js
@@ -1,37 +1,37 @@
 import React, { PropTypes } from 'react'
 
 class Html extends React.Component {
-
-  static propTypes = {
-    title: PropTypes.string,
-    description: PropTypes.string,
-    body: PropTypes.string.isRequired,
-    entry: PropTypes.string.isRequired
-  }
-
-  static defaultProps = {
-    title: '',
-    description: ''
-  }
-
   render() {
     return (
       <html>
-      <head>
-        <meta charSet="utf-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <title>{this.props.title}</title>
-        <meta name="description" content={this.props.description} />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link type="text/css" rel="stylesheet" href="/style.css" />
-      </head>
-      <body>
-        <div id="app" dangerouslySetInnerHTML={{ __html: this.props.body }} />
-        <script src={this.props.entry}></script>
-      </body>
+        <head>
+          <meta charSet="utf-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <title>{this.props.title}</title>
+          <meta name="description" content={this.props.description} />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link type="text/css" rel="stylesheet" href="/style.css" />
+        </head>
+        <body>
+          <div id="app" dangerouslySetInnerHTML={{ __html: this.props.body }} />
+          <script src={this.props.entry}></script>
+        </body>
       </html>
     )
   }
 }
+
+Html.propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string,
+  body: PropTypes.string.isRequired,
+  entry: PropTypes.string.isRequired
+}
+
+Html.defaultProps = {
+  title: '',
+  description: ''
+}
+
 
 export default Html
